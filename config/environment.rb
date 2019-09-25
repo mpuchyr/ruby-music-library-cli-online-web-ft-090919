de18e27b7bc1dad50c4de42940606aa8acc7669b
Matt Puchyr <matt.puchyr@gmail.com>
2019-09-25 02:20:32 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,19 @@ require 'bundler'
 Bundler.require
 
 module Concerns
+  module Findable
+    def find_by_name(item_name)
+      self.all.detect {|item| item.name == item_name}
+    end
+
+    def find_or_create_by_name(item_name)
+      item = self.find_by_name(item_name)
+      if !item
+        item = self.create(item_name)
+      end
+      item
+    end
+  end
 end
 
 require_all 'lib'
